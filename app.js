b3540a37f347723f2ecf1ecea33de5ad77d47a51
Ömer Can <omercanhocaoglu@gmail.com>
2025-02-18 17:15:13 +0300

    delete işlemi yapılmıştır

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
 // connect DB
 mongoose.connect('mongodb://localhost/freelancer-test-db',{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 // Pages
@@ -76,6 +79,15 @@ app.put('/photos/:id', async (req, res) => {
     photoUpdate.description = req.body.description;
     photoUpdate.save();
     res.redirect(`/photos/${req.params.id}`);
+});
+// DELETE
+app.delete('/photos/:id',async ( req, res ) => {
+    // console.log(req.params.id);
+    const photo = await Photo.findOne({ _id: req.params.id });
+    let deletedImage = __dirname + '/public' + photo.image;
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndRemove( req.params.id );
+    res.redirect('/');
 });
 
 const port = 3000;
